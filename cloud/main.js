7e73fde915598033637a54990bd52417fac5b730
gronour <nibman@gmail.com>
2016-05-07 22:02:48 -0400

    Added cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -6,5 +6,18 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.afterSave("PriceList", function(request)
 {
   console.log("PriceList was saved");
-  console.log(request);
+  console.log(request.object.id);
+
+  query = new Parse.Query("PriceList");
+  query.get(request.object.id,
+      {
+        success: function(object) {
+          console.log(object);
+        },
+
+        error: function(object, error) {
+          // error is an instance of Parse.Error.
+        }
+      });
+  
 })
